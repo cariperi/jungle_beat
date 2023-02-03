991c5ea34c0de2d3ead929a9a4800c6bcd1579bd
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-02 21:52:06 -0500

    test: add tests for initialize to node class

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,2 +1,25 @@
 require 'rspec'
 require './lib/node'
+
+describe Node do
+
+  describe '#initialize' do
+    it 'exists' do
+      node = Node.new('plop')
+      
+      expect(node).to be_a Node
+    end
+
+    it 'has data' do
+      node = Node.new('plop')
+
+      expect(node.data).to eq('plop')
+    end
+
+    it 'has a next node is nil by default' do
+      node = Node.new('plop')
+
+      expect(node.next_node).to eq(nil)
+    end
+  end
+end
