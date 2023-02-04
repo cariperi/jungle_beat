2b5eea9c119d4a9490aefaab9634ecac4fe97a46
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-04 16:33:21 -0500

    test: add test for initialize method to JungleBeat class

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -4,5 +4,18 @@ require './lib/linked_list'
 require './lib/node'
 
 describe JungleBeat do
+  describe '#initialize' do
+    it 'exists' do
+      jb = JungleBeat.new
 
+      expect(jb).to be_a JungleBeat
+    end
+
+    it 'has an empty LinkedList object' do
+      jb = JungleBeat.new
+
+      expect(jb.list).to be_a LinkedList
+      expect(jb.list.head).to eq(nil)
+    end
+  end
 end
