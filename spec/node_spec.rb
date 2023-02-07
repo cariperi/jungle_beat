307cc0e964069c23419a51e83d74bc3a111d603e
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-06 23:28:42 -0500

    refactor and edit tests for Node, LinkedList, and JungleBeat classes

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,24 +2,21 @@ require 'rspec'
 require './lib/node'
 
 describe Node do
+  before(:each) do
+    @node = Node.new('plop')
+  end
 
   describe '#initialize' do
     it 'exists' do
-      node = Node.new('plop')
-      
-      expect(node).to be_a Node
+      expect(@node).to be_a Node
     end
 
     it 'has data' do
-      node = Node.new('plop')
-
-      expect(node.data).to eq('plop')
+      expect(@node.data).to eq('plop')
     end
 
     it 'has a next node that is nil by default' do
-      node = Node.new('plop')
-
-      expect(node.next_node).to eq(nil)
+      expect(@node.next_node).to eq(nil)
     end
   end
 end
